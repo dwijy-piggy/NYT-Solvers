c4d7159001442b4052456c16fdcfa5e2aa344cbe
dwijy-piggy <45340809+dwijy-piggy@users.noreply.github.com>
2023-05-21 13:56:31 -0500

    Update NYT-Digits.js

--- NYT-Digits.js
+++ NYT-Digits.js
@@ -1,14 +1,15 @@
 javascript:var f = () => fetch("https://www.nytimes.com/games/prototype/digits/dist/index.c0e9c2d7.js").then(r => r.text()).then(r => {
-    let a = eval(r.slice(r.indexOf(",or=")+4, r.indexOf("]}]")+3));
-    let b = [...document.querySelectorAll("[id^='target']")].slice(0,5).map(x => parseInt(x.innerText));
-	document.querySelectorAll("[id^='history-container']").forEach(x => x.appendChild(document.createTextNode("Answer: " + a.find(x => x.targets.every((e, i) => e === b[i])).solutions[parseInt(document.getElementsByClassName("puzzle active")[0].id[2])])));
+    let a = eval(r.slice(r.indexOf(",or=") + 4, r.indexOf("]}]") + 3));
+    let b = [...document.querySelectorAll("[id^='target']")].slice(0, 5).map(x => parseInt(x.innerText));
+    document.querySelectorAll("[id^='history-container']").forEach(x => x.appendChild(document.createTextNode("Answer: " + a.find(x => x.targets.every((e, i) => e === b[i])).solutions[parseInt(document.getElementsByClassName("puzzle active")[0].id[2])])));
 });
 f();
 new MutationObserver((ml) => {
-  for (const m of ml) {
-    if (m.type === "attributes" && m.attributeName == "class"){
-		document.querySelectorAll("[id^='history-container']").forEach(x => {if(x.lastChild.tagName != "ul")x.removeChild(x.lastChild);});
-		f();
-		break;
-	}
-  }}).observe(document.getElementById("puzzles"), {attributes: true, subtree: true});
+    for (const m of ml) {
+        if (m.type === "attributes" && m.attributeName == "class") {
+            document.querySelectorAll("[id^='history-container']").forEach(x => {if (x.lastChild.tagName != "ul") x.removeChild(x.lastChild);});
+            f();
+            break;
+        }
+    }
+}).observe(document.getElementById("puzzles"), {attributes: true, subtree: true});
