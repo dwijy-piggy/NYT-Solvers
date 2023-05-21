4056f3e40600261d3c1c052e8b66b5b53765e054
dwijy-piggy <45340809+dwijy-piggy@users.noreply.github.com>
2023-05-21 02:10:51 -0500

    Create NYT-Crossword.js
    Changes hints to respective answers

---
+++ NYT-Crossword.js
@@ -0,0 +1,11 @@
+javascript: var split = window.location.href.split("/");
+fetch("https://www.nytimes.com/svc/crosswords/v6/puzzle/daily/" + split.slice(split.length - 3).join("-") + ".json")
+    .then(response => response.text())
+    .then(x => {
+        hintplacement = JSON.parse(x.slice(x.indexOf("\"clues\":[{") + 8, x.indexOf("}]}]") + 4)).map(y => y["cells"]);
+        hints = document.getElementsByClassName("xwd__clue--text xwd__clue-format");
+        letters = JSON.parse(x.slice(x.indexOf("s\":") + 3, x.indexOf("}]") + 2)).map(y => y["answer"]);
+    })
+    .then(() => {
+        for (var x in hintplacement) hints[x].innerHTML = hintplacement[x].map(y => letters[y]).join("");
+    });
