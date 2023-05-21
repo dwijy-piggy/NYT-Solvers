6ec02a3cd6df12e324dd55b789c54821e6443e13
37382 <134091298+37382@users.noreply.github.com>
2023-05-21 15:41:42 -0500

    Update Crossword.js

--- Crossword.js
+++ Crossword.js
@@ -1,4 +1,4 @@
-javascript:[...document.querySelectorAll('g.xwd__cell')].forEach(e => {
+javascript:document.querySelectorAll('g.xwd__cell').forEach(e => {
     e.appendChild(document.createElement('p')).click();
     a = e[Object.keys(e).filter(k => k.startsWith('__reactInternalInstance'))].return.pendingProps.answer;
     e.dispatchEvent(new KeyboardEvent('keypress', {bubbles: true, charCode: a.charCodeAt(0)}));
