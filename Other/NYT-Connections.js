3b3e9f0450c7badb7a4b1068cd760a652147ffb4
dwijy-piggy <45340809+dwijy-piggy@users.noreply.github.com>
2024-01-10 13:00:31 -0600

    Update NYT-Connections.js

--- Other/NYT-Connections.js
+++ Other/NYT-Connections.js
@@ -1,10 +1,1 @@
-javascript:function eq(a, b){
-    return a.length == b.length && a.every((e, i) => e === b[i]);
-}
-var arr = Array.from(document.querySelectorAll("[id^='item-']")).map(x => x.innerText);
-fetch("https://www.nytimes.com/games/prototype/connections/dist/index.15c9846b.js").then(r => r.text()).then(r => {
-    let o = eval(r.slice(r.indexOf("var F")+6, r.indexOf("]}]")+3)).map(y => [y, y["startingGroups"][0].concat(...y["startingGroups"].slice(1))]).find(y => eq(y[1], arr))[0]["groups"];
-    p = document.createElement('pre');
-    for(let x of Object.keys(o)) p.textContent += (`${x}: ${o[x]["members"].join(', ')}\n`);
-    document.body.appendChild(p);
-});
+document.getElementsByClassName("pz-game-field")[0].appendChild(document.createTextNode(Object.values(gameData[gameData.length-1].groups).map(x => x.members.toString()).join('  -  ')))
